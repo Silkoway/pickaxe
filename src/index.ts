dcf0ecc76a19e367f8ea24c45059cb08de593bce
Silkoway <ethancai920@gmail.com>
2022-10-30 22:35:07 +0800

    ok fixed error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
 import { err } from './utils'
 import fs from 'fs'
+import { Lexer } from './lexer'
 const argv: any = yargs(hideBin(process.argv)).argv
 
 let filename = argv._[0]
@@ -11,7 +12,7 @@ if (filename === undefined) {
     err("File path not specified. Use it like this: `sslc {filepath}`")
 }
 if (fs.existsSync(filename)) {
-    let lexer = new Lexer()
+    let lexer = new Lexer(fs.readFileSync(filename, 'utf8'))
 } else {
     err("File path specified does not lead to an existing file. Are you sure you typed the right path?")
 }
